40f6be552c0e0117dd2f5ad8307bbe6e6783de65
Galip <Galip@DESKTOP-HRK9HK1>
2021-03-20 14:47:41 +0300

    Inheritance and Interface demo, example added

---
+++ Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FisrtName = "Galip"},
+                new Student{FisrtName = "Ali"}, 
+                new Person{FisrtName = "Ahmet"}
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FisrtName);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    class Person2
+    {
+        
+    }
+
+    class Customer:Person
+    {
+        public string Cİty { get; set; }
+    }
+
+    class Student:Person
+    {
+        public string Departmant { get; set; }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        //interface new lenemez.
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+
+
+            IPersonManager employeeManager = new EmployeeManager();
+
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new CustomerManager());
+            projectManager.Add(new EmployeeManager());
+            projectManager.Add(new InternManager());
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+
+    }
+
+    //class - inherits -------- interface - implements yazılır
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //Müşteri ekleme kodları
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //Personel ekleme kodları
+            Console.WriteLine("Personel Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Güncellendi");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
