ca4045ee773edd971ede6cc146410252b17cac70
Galip <Galip@DESKTOP-HRK9HK1>
2021-03-26 15:55:01 +0300

    Added LINQ example

--- AbstractClass/Program.cs
+++ AbstractClass/Program.cs
@@ -6,8 +6,8 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            Employee emp1 = new PermanentEmployee("267543", "Tom", 5000);
-            Employee emp2 = new ContractEmployee("953456", "Ketty", 30);
+            Employee emp1 = new PermanentEmployee("267543", "Galip", 5000);
+            Employee emp2 = new ContractEmployee("953456", "Ayşe", 30);
             Console.WriteLine(emp1);
             Console.WriteLine(emp2);
         }
